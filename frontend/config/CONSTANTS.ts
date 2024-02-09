c80c21893a56d457a5d197ad1037cfcd261001c0
yashUcr773 <yagga004@ucr.edu>
2024-02-09 05:59:59 -0800

    minor fix

--- frontend/config/CONSTANTS.ts
+++ frontend/config/CONSTANTS.ts
@@ -1,4 +1,4 @@
 export const CONSTANTS = {
-    APIBASEURL: "https:/shtr.dev/api/v1",
-    PRODURL: "https:/shtr.dev/"
+    APIBASEURL: "https://shtr.dev/api/v1",
+    PRODURL: "https://shtr.dev/"
 };
