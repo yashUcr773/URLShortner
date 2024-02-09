1fe697dc7c7ffb0582c6767aa6568d4d78123360
yashUcr773 <yagga004@ucr.edu>
2024-02-09 05:52:48 -0800

    minor update for build

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-
 export function Loader({ fullpage }: any) {
+    
+    if (fullpage) {
+        console.log('update')
+    }
+
     return (
         <div
             className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
