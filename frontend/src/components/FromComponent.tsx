cc3d8a9944a73be1258edefcdf0ca6e351d8e88d
yashUcr773 <yagga004@ucr.edu>
2024-02-09 18:59:06 -0800

    added copy and launch buttons

--- frontend/src/components/FromComponent.tsx
+++ frontend/src/components/FromComponent.tsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+import { Copy } from "../assets/Copy";
+import { Launch } from "../assets/Launch";
+import { Tooltip } from 'react-tooltip'
+
 export function FormComponent({ formLabel, placeHolder, formValue, formSetter, readonly }: any) {
+
+    const [copy, setCopy] = useState(true)
+
+    function copyLink() {
+        navigator.clipboard.writeText(formValue)
+        setCopy(false)
+        setTimeout(() => {
+            setCopy(true)
+        }, 2000)
+    }
+
+    function launchLink() {
+        window.open(formValue, '_blank', 'noreferrer')
+    }
 
     return <div className="form-component flex flex-col gap-2 w-full">
         <label className="font-semibold text-xl">{formLabel}</label>
-        <input className={`border border-black rounded-lg p-4 ${readonly? "bg-gray-200":""}`} placeholder={placeHolder} value={formValue} onChange={(e) => formSetter(e.target.value)} readOnly={readonly}></input>
+        <div className="w-full flex flex-row gap-2">
+
+            <input className={`border border-black w-full rounded-lg p-4 ${readonly ? "bg-gray-200" : ""}`} placeholder={placeHolder} value={formValue} onChange={(e) => formSetter(e.target.value)} readOnly={readonly}></input>
+            {formLabel == 'ShortenedURL' ?
+                <div className="flex flex-row gap-2 p-2">
+                    <Tooltip id="my-tooltip" />
+                    <button onClick={(e) => { copyLink() }} className="border border-black p-2 rounded-lg bg-gray-200"><a data-tooltip-id="my-tooltip" data-tooltip-content={copy ? 'Copy' : 'Copied'}><Copy /></a></button>
+                    <button onClick={(e) => { launchLink() }} className="border border-black p-2 rounded-lg bg-gray-200"> <a data-tooltip-id="my-tooltip" data-tooltip-content='Launch Link'><Launch /></a></button>
+                </div> :
+                null}
+        </div>
     </div>
 }
--- frontend/src/components/ResultComponent.tsx
+++ frontend/src/components/ResultComponent.tsx
@@ -2,6 +2,8 @@ import { FormComponent } from "./FromComponent"
 import { useRecoilValue, useSetRecoilState } from "recoil"
 import { URLSatom, flipAtom } from "../store/atom/URLS"
 import { CONSTANTS } from "../../config/CONSTANTS"
+import { Copy } from "../assets/Copy"
+import { Launch } from "../assets/Launch"
 
 export function ResultComponent() {
 
@@ -10,7 +12,8 @@ export function ResultComponent() {
 
     return <div className="m-8 form-container w-full sm:w-[500px] bg-emerald-200 border border-black rounded-lg shadow-lg flex flex-col gap-8 p-8 items-center justify-center">
         <FormComponent formLabel={'GeneratedURL'} formValue={urls.completeURL} readonly={true}></FormComponent>
-        <FormComponent formLabel={'ShortenedURL'} formValue={CONSTANTS.PRODURL+urls.shortenedURL} readonly={true}></FormComponent>
+        <FormComponent formLabel={'ShortenedURL'} formValue={CONSTANTS.PRODURL + urls.shortenedURL} readonly={true}></FormComponent>
+
         <button onClick={() => { setFlip(false) }} className="border border-black p-4 w-full bg-black text-white  text-lg rounded-lg  flex flex-row items-center justify-center h-16 gap-4">Generate More</button>
     </div>
 
