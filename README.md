ac47b817ff850be52d576f2420d1b50d7e902e83
yashUcr773 <yagga004@ucr.edu>
2024-02-10 04:52:06 -0800

    added header links

--- README.md
+++ README.md
@@ -8,6 +8,12 @@
 - React, Recoil and Tailwind for FE
 - Express, Nodejs, MongoDB for BE
 
+To run in dev mode
+- Clone the repo
+- Run `npm install`
+- `cd frontend` and Run `npm run run-both`
+- Frontend will start on localhost:5173 and Backend will start at localhost:3000
+
 
 FE hosted on https://app.shtr.dev
 BE hosted on https://shtr.dev
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,16 @@
 import { useNavigate } from "react-router-dom"
 import { useSetRecoilState } from "recoil"
 import { flipAtom } from "../store/atom/URLS"
+import { HeaderLinks } from "./HeaderLinks"
 
 export function Header() {
     const navigate = useNavigate()
     const setFlip = useSetRecoilState(flipAtom)
 
     return <header onClick={() => { navigate('/'); setFlip(false) }}
-        className="bg-emerald-950 w-full h-20 flex  cursor-pointer justify-center items-center text-white text-4xl font-bold tracking-wide">URL Shortner</header>
+        className="bg-emerald-950 w-full h-20 flex px-2 sm:px-10 cursor-pointer justify-between items-center text-white text-4xl font-bold tracking-wide">
+        <div></div>
+        URL Shortner
+        <HeaderLinks></HeaderLinks>
+    </header>
 }
